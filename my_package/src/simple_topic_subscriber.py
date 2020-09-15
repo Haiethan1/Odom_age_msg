b2994a1ffebf227a5900792f57c977d05587a628
ethan li <ethan.jeffrey@gmail.com>
2020-09-15 19:00:54 +0000

    publishes the odom topic

---
+++ my_package/src/simple_topic_subscriber.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int32
+from nav_msgs.msg import Odometry
+
+def callback(msg): 
+  print msg.pose
+
+rospy.init_node('topic_subscriber')
+sub = rospy.Subscriber('odom', Odometry, callback)
+rospy.spin()
